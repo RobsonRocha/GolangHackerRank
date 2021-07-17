4f2c516eb21437cb866c02957a68ee6c0576bae1
Robson <robson.rocha@m4u.com.br>
2021-07-17 19:28:35 -0300

    Novo desafio Balanced Brackets.

---
+++ BalancedBrackets/Solution.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+type Stack []string
+
+// IsEmpty: check if stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+// Push a new value onto the stack
+func (s *Stack) Push(str string) {
+	*s = append(*s, str) // Simply append the new value to the end of the stack
+}
+
+// Remove and return top element of stack. Return false if stack is empty.
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	} else {
+		index := len(*s) - 1 // Get the index of the top most element.
+		element := (*s)[index] // Index into the slice and obtain the element.
+		*s = (*s)[:index] // Remove it from the stack by slicing it off.
+		return element, true
+	}
+}
+
+
+func isBalanced(s string) string {
+
+	expectedMap := make(map[string]string)
+
+	expectedMap["("] = ")"
+	expectedMap["{"] = "}"
+	expectedMap["["] = "]"
+
+	// Write your code here
+	result := "YES"
+	var stack Stack
+
+	expected := ""
+
+	for _, v :=range s{
+		value, found := expectedMap[string(v)]
+		if found {
+			stack.Push(value)
+		}else {
+			f := true
+			expected, f = stack.Pop()
+
+			if !f {
+				return "NO"
+			}
+
+			if string(v) != expected{
+				return "NO"
+			}
+		}
+	}
+
+	if !stack.IsEmpty(){
+		return "NO"
+	}
+
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+	tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		s := readLine(reader)
+
+		result := isBalanced(s)
+
+		fmt.Fprintf(writer, "%s\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
